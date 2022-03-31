06211cfd0a47ca9e0d144d98219ef1abf7844366
Brian Campbell <campbellsoupbs@gmail.com>
2022-03-31 14:34:32 -0400

    4_kata commit

---
+++ kata/4_kata.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#You are going to be given a word. Your job is to return the middle character of the word. 
+# If the word's length is odd, return the middle character. 
+# If the word's length is even, return the middle 2 characters.
+
+def get_middle(s):
+    if len(s)%2 == 1:
+        return s[int(len(s)/2 - 0.5)]
+    else:
+        return '{}{}'.format(s[int(len(s)/2 -1)], s[int(len(s)/2)])
+
+
